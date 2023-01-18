59e3eeb6d9b748ede1969c26b39792ebac27ada9
andrew-yip <andrew.yip22@gmail.com>
2023-01-18 00:00:55 -0800

    added express routes

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+const dotenv = require('dotenv')
+const express = require('express')
 
-async function callApi() {
+dotenv.config()
+
+// create a simple express api that calls the function above
+const app = express()
+const PORT = 3000 // server port - 3000 is typically used for react
+
+app.post('/', async (req, res) => {
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: "Say this is a test",
         max_tokens: 7,
         temperature: 0,
     });
-}
+    console.log(response.data.choices[0].text)
+    res.json({
+        data: response.data
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`App is listening at http://localhost:${PORT}`)
+})
